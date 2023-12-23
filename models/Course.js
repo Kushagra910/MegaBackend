4fd887d7188ea89c17999534e87680daae7fdee6
Kushagra910 <kushagram63@gmail.com>
2023-12-23 06:33:07 +0530

    Added tag field in course model

--- models/Course.js
+++ models/Course.js
@@ -40,6 +40,10 @@ const courseSchema = new mongoose.Schema({
     type :String,
     required:true,
   },
+  tag:{
+    type:[String],
+    required:true,
+  },
   category :{
     type : mongoose.Schema.Types.ObjectId,
     ref : "Category",
