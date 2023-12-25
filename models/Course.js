7631ef08d3cb80bd01e41cca5f397ee12fa0e2cf
Kushagra910 <kushagram63@gmail.com>
2023-12-25 05:52:44 +0530

    Added two fields for Status and Instructions

--- models/Course.js
+++ models/Course.js
@@ -55,6 +55,13 @@ const courseSchema = new mongoose.Schema({
       required : true,
     }
   ],
+  instructions:{
+    type:[String],
+  },
+  status:{
+    type:String,
+    enum:["Draft","Published"],
+  }
 });
 
 module.exports = mongoose.model("Course",courseSchema);
