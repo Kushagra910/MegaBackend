5ea048a47a104f5b2d620c1a6587cb09fedf5cfb
Kushagra910 <kushagram63@gmail.com>
2024-02-08 20:02:59 +0530

    Updated getCategories handler

--- controllers/Category.js
+++ controllers/Category.js
@@ -1,4 +1,9 @@
 const Category = require("../models/Category");
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max)
+}
+
 // create Tag handler
 exports.createCategory = async(req,res)=>{
   try{
@@ -50,59 +55,76 @@ exports.showAllCategories = async(req,res)=>{
 // categoryPageDetails shows most popular,top courses & frequentyly bought courses 
 exports.categoryPageDetails = async (req, res) => {
 	try {
-		const { categoryId } = req.body;
-
+		const { categoryId } = req.body
+		// console.log("PRINTING CATEGORY ID: ", categoryId);
 		// Get courses for the specified category
 		const selectedCategory = await Category.findById(categoryId)
-			.populate("courses")
-			.exec();
-		console.log(selectedCategory);
+			.populate({
+				path: "courses",
+				match: { status: "Published" },
+				populate: "ratingAndReviews",
+			})
+			.exec()
+
+		//console.log("SELECTED COURSE", selectedCategory)
 		// Handle the case when the category is not found
 		if (!selectedCategory) {
-			console.log("Category not found.");
+			console.log("Category not found.")
 			return res
 				.status(404)
-				.json({ success: false, message: "Category not found" });
+				.json({ success: false, message: "Category not found" })
 		}
 		// Handle the case when there are no courses
-		// if (selectedCategory.courses.length === 0) {
-		// 	console.log("No courses found for the selected category.");
-		// 	return res.status(404).json({
-		// 		success: false,
-		// 		message: "No courses found for the selected category.",
-		// 	});
-		// }
+		if (selectedCategory.courses.length === 0) {
+			console.log("No courses found for the selected category.")
+			return res.status(404).json({
+				success: false,
+				message: "No courses found for the selected category.",
+			})
+		}
 
-		// const selectedCourses = selectedCategory.courses;
-
-		// // Get courses for other categories
-		// const categoriesExceptSelected = await Category.find({
-		// 	_id: { $ne: categoryId }, // ne = not equal to
-		// }).populate("courses");
-		// let differentCourses = [];
-		// for (let category of categoriesExceptSelected) {
-		// 	differentCourses.push(...category.courses);
-		// }
-
-		// // Get top-selling courses across all categories
-		// const allCategories = await Category.find({}).populate("courses");
-		// const allCourses = allCategories.flatMap((category) => category.courses);
-		// const mostSellingCourses = allCourses
-		// 	.sort((a, b) => b.sold - a.sold)
-		// 	.slice(0, 10);
-
-		return res.status(200).json({
-			success:true,
-			message:"Category Page Details are Coming"
-			// selectedCourses: selectedCourses,
-			// differentCourses: differentCourses,
-			// mostSellingCourses: mostSellingCourses,
-		});
+		// Get courses for other categories
+		const categoriesExceptSelected = await Category.find({
+			_id: { $ne: categoryId },
+		})
+		let differentCategory = await Category.findOne(
+			categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+				._id
+		)
+			.populate({
+				path: "courses",
+				match: { status: "Published" },
+			})
+			.exec()
+			//console.log("Different COURSE", differentCategory)
+		// Get top-selling courses across all categories
+		const allCategories = await Category.find()
+			.populate({
+				path: "courses",
+				match: { status: "Published" },
+				populate: {
+					path: "instructor",
+			},
+			})
+			.exec()
+		const allCourses = allCategories.flatMap((category) => category.courses)
+		const mostSellingCourses = allCourses
+			.sort((a, b) => b.sold - a.sold)
+			.slice(0, 10)
+		 // console.log("mostSellingCourses COURSE", mostSellingCourses)
+		res.status(200).json({
+			success: true,
+			data: {
+				selectedCategory,
+				differentCategory,
+				mostSellingCourses,
+			},
+		})
 	} catch (error) {
 		return res.status(500).json({
 			success: false,
 			message: "Internal server error",
 			error: error.message,
-		});
+		})
 	}
-};
+}
